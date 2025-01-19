d3b1542ad054b0fd6dc92529b8474db78e42e513
Abin Roy <abinroy510@gmail.com>
2025-01-19 07:44:30 +0600

    order shipping  logic update

--- Ecommerce/store/models.py
+++ Ecommerce/store/models.py
@@ -34,6 +34,18 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
     @property
+    def shipping(self):
+        shipping = False
+        orderitems =  self.order_item_set.all()
+        for i in orderitems:
+            if i.product.digital==False:
+                shipping=True
+            return shipping
+        
+
+
+
+    @property
     def get_cart_total(self):
         orderitems = self.order_item_set.all()
         total = sum([item.get_total for item in orderitems])
